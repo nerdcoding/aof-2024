157ac522f6ab4b116fbb41a1af24926171b38ff8
Tobias Koltsch <tobias@koltsch.de>
2024-12-06 17:38:29 +0100

    Add day02 - part2

--- src/main/kotlin/org/nerdcoding/aof2024/day02/Day02.kt
+++ src/main/kotlin/org/nerdcoding/aof2024/day02/Day02.kt
@@ -8,25 +8,37 @@ private const val INPUT_FILE_LOCATION = "./src/main/resources/day02/input.txt"
 fun main() {
     val reports = readInputFile();
 
-    val safeReports = countSafeReportsPart1(reports)
+    val safeReportsPart1 = countSafeReportsPart1(reports)
+    val safeReportsPart2 = countSafeReportsPart2(reports)
 
-    println("Part 1 safe reports: $safeReports")
+    println("Part 1 safe reports: $safeReportsPart1")
+    println("Part 2 safe reports: $safeReportsPart2")
 }
 
 private fun readInputFile(): List<List<Int>> = File(INPUT_FILE_LOCATION).useLines {
-    lines -> lines.map {
-        singleLine -> singleLine.split("\\s+".toRegex())
-            .map { it.toInt() }
-    }.toList()
+        lines -> lines.map {
+            singleLine -> singleLine.split("\\s+".toRegex())
+                .map { it.toInt() }
+        }.toList()
 }
 
 private fun countSafeReportsPart1(reports: List<List<Int>>): Int {
-    return reports.count { report -> isValidReport(report) }
+    return reports.count { report -> isValidReportPart1(report) }
 }
 
-private fun isValidReport(report: List<Int>): Boolean {
+private fun countSafeReportsPart2(reports: List<List<Int>>): Int {
+    return reports.count { report -> isValidReportPart2(report) }
+}
+
+private fun isValidReportPart1(report: List<Int>): Boolean {
     return (areIncreasingLevels(report) || areDecreasingLevels(report))
             && isValidAdjacentLevelDifference(report)
+}
+
+private fun isValidReportPart2(report: List<Int>): Boolean {
+    return report.indices
+        .map { toRemove -> report.filterIndexed { i, _ -> i != toRemove } }
+        .any { isValidReportPart1(it) }
 }
 
 private fun areIncreasingLevels(report: List<Int>): Boolean {
@@ -38,7 +50,7 @@ private fun areDecreasingLevels(report: List<Int>): Boolean {
 }
 
 private fun isValidAdjacentLevelDifference(
-        report: List<Int>): Boolean {
+    report: List<Int>): Boolean {
 
     return report.zipWithNext()
         .all { (a, b) -> abs(a - b) in 1..3 }
