13ea5e60698b5618afe72783bf607191bc972fbd
Tobias Koltsch <tobias@koltsch.de>
2024-12-07 16:30:22 +0100

    Add day04 - part2

--- src/main/kotlin/org/nerdcoding/aof2024/day04/Day04.kt
+++ src/main/kotlin/org/nerdcoding/aof2024/day04/Day04.kt
@@ -7,10 +7,21 @@ private const val INPUT_FILE_LOCATION = "./src/main/resources/day04/input.txt"
 private val wordSearch = WordSearch(readInputFile())
 
 fun main() {
+    println("XMAS count part1: ${countXmasPart1()}")
+    println("XMAS count part2: ${countXmasPart2()}")
+}
 
-    val allPositionsOfX = findAllPositionsOfX()
+private fun readInputFile(): Array<CharArray> =
+    File(INPUT_FILE_LOCATION)
+        .useLines {
+            lines -> lines.map {
+                singleLine -> singleLine.toCharArray()
+            }.toList().toTypedArray()
+    }
+
+private fun countXmasPart1(): Int {
     var xmasCount = 0;
-    for (positionOfX in allPositionsOfX) {
+    for (positionOfX in findAllPositionsOfChar('X')) {
         if (checkRight(positionOfX, 'M')) {
             xmasCount++
         }
@@ -36,24 +47,43 @@ fun main() {
             xmasCount++
         }
     }
-
-    println("XMAS count: $xmasCount")
+    return xmasCount
 }
 
-private fun readInputFile(): Array<CharArray> =
-    File(INPUT_FILE_LOCATION)
-        .useLines {
-            lines -> lines.map {
-                singleLine -> singleLine.toCharArray()
-            }.toList().toTypedArray()
-    }
+private fun countXmasPart2(): Int {
+    /*
+     1      2      3      4
+    M M    M S    S S    S M
+     A      A      A      A
+    S S    M S    M M    S M
 
-private fun findAllPositionsOfX(): List<Pair<Int, Int>> {
+    MMSS   MSMS   SSMM   SMSM
+    */
+
+    var xmasCount = 0;
+    for (positionOfA in findAllPositionsOfChar('A')) {
+        if (isMMSS(positionOfA)) {
+            xmasCount++
+        }
+        if (isMSMS(positionOfA)) {
+            xmasCount++
+        }
+        if (isSSMM(positionOfA)) {
+            xmasCount++
+        }
+        if (isSMSM(positionOfA)) {
+            xmasCount++
+        }
+    }
+    return xmasCount
+}
+
+private fun findAllPositionsOfChar(char: Char): List<Pair<Int, Int>> {
     val result = mutableListOf<Pair<Int, Int>>()
     for (y in 0 until wordSearch.getSize()) {
         for (x in 0 until wordSearch.getSize()) {
             val point = Pair(x, y)
-            if (wordSearch.getCharAtPoint(point) == 'X') {
+            if (wordSearch.getCharAtPoint(point) == char) {
                 result.add(point)
             }
         }
@@ -166,7 +196,6 @@ private fun checkDownLeft(startPoint: Pair<Int, Int>, searchChar: Char): Boolean
     return false;
 }
 
-
 private fun findNextChar(searchChar: Char): Char? =
     when (searchChar) {
         'X' -> 'M'
@@ -174,3 +203,64 @@ private fun findNextChar(searchChar: Char): Char? =
         'A' -> 'S'
         else -> null
     }
+
+private fun isMMSS(startPoint: Pair<Int, Int>): Boolean =
+    checkSingleUpLeft(startPoint, 'M')
+            && checkSingleUpRight(startPoint, 'M')
+            && checkSingleDownLeft(startPoint, 'S')
+            && checkSingleDownRight(startPoint, 'S')
+
+private fun isMSMS(startPoint: Pair<Int, Int>): Boolean =
+    checkSingleUpLeft(startPoint, 'M')
+            && checkSingleUpRight(startPoint, 'S')
+            && checkSingleDownLeft(startPoint, 'M')
+            && checkSingleDownRight(startPoint, 'S')
+
+private fun isSSMM(startPoint: Pair<Int, Int>): Boolean =
+    checkSingleUpLeft(startPoint, 'S')
+            && checkSingleUpRight(startPoint, 'S')
+            && checkSingleDownLeft(startPoint, 'M')
+            && checkSingleDownRight(startPoint, 'M')
+
+private fun isSMSM(startPoint: Pair<Int, Int>): Boolean =
+    checkSingleUpLeft(startPoint, 'S')
+            && checkSingleUpRight(startPoint, 'M')
+            && checkSingleDownLeft(startPoint, 'S')
+            && checkSingleDownRight(startPoint, 'M')
+
+
+private fun checkSingleUpRight(startPoint: Pair<Int, Int>, searchChar: Char): Boolean {
+    val movedPoint = wordSearch.moveInDirection(
+        wordSearch.moveInDirection(startPoint, WordSearch.Direction.UP),
+        WordSearch.Direction.RIGHT
+    )
+
+    return wordSearch.getCharAtPoint(movedPoint) == searchChar
+}
+
+private fun checkSingleUpLeft(startPoint: Pair<Int, Int>, searchChar: Char): Boolean {
+    val movedPoint = wordSearch.moveInDirection(
+        wordSearch.moveInDirection(startPoint, WordSearch.Direction.UP),
+        WordSearch.Direction.LEFT
+    )
+
+    return wordSearch.getCharAtPoint(movedPoint) == searchChar
+}
+
+private fun checkSingleDownRight(startPoint: Pair<Int, Int>, searchChar: Char): Boolean {
+    val movedPoint = wordSearch.moveInDirection(
+        wordSearch.moveInDirection(startPoint, WordSearch.Direction.DOWN),
+        WordSearch.Direction.RIGHT
+    )
+
+    return wordSearch.getCharAtPoint(movedPoint) == searchChar
+}
+
+private fun checkSingleDownLeft(startPoint: Pair<Int, Int>, searchChar: Char): Boolean {
+    val movedPoint = wordSearch.moveInDirection(
+        wordSearch.moveInDirection(startPoint, WordSearch.Direction.DOWN),
+        WordSearch.Direction.LEFT
+    )
+
+    return wordSearch.getCharAtPoint(movedPoint) == searchChar
+}
