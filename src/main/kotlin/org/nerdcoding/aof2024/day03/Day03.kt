a0fa901f4130cf2e1ba6e4aba918051cfb4702d9
Tobias Koltsch <tobias@koltsch.de>
2024-12-07 12:47:57 +0100

    Add day03 - part2

--- src/main/kotlin/org/nerdcoding/aof2024/day03/Day03.kt
+++ src/main/kotlin/org/nerdcoding/aof2024/day03/Day03.kt
@@ -3,29 +3,40 @@ package org.nerdcoding.aof2024.day03
 import java.io.File
 
 private const val INPUT_FILE_LOCATION = "./src/main/resources/day03/input.txt"
-private val REGEX = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+private val FIND_MUL_REGEX = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+private val DELETE_DONT_REGEX = Regex("""don't\(\).*?(?:do\(\)|$)""")
 
 fun main() {
-    val multiplication = readMultiplicationsFromInputFile();
-    val part1Result = calculateResultPart1(multiplication)
 
+    val inputFile = readFileAsString()
+    val multiplicationsPart1 = searchMultiplications(inputFile)
+    val part1Result = calculateResult(multiplicationsPart1)
     println("Part 1 result: $part1Result")
-    //println("Part 2 safe reports: ")
+
+    val multiplicationsPart2 = searchMultiplications(
+        removeDonts(inputFile)
+    )
+    val part2Result = calculateResult(multiplicationsPart2)
+    println("Part 2 result: $part2Result")
 }
 
-private fun readMultiplicationsFromInputFile(): List<Pair<Int, Int>> = File(INPUT_FILE_LOCATION).useLines {
-    lines -> lines.flatMap {
-        singleLine -> REGEX.findAll(singleLine)
-            .map { match ->
-                val x = match.groupValues[1].toInt()
-                val y = match.groupValues[2].toInt()
-                Pair(x,y)
-            }
-    }.toList()
-}
+private fun readFileAsString(): String =
+    File(INPUT_FILE_LOCATION).readText()
 
-private fun calculateResultPart1(multiplications: List<Pair<Int, Int>>): Int {
-    return multiplications
+private fun searchMultiplications(input: String): List<Pair<Int, Int>> =
+    FIND_MUL_REGEX.findAll(input)
+        .map { match ->
+            val x = match.groupValues[1].toInt()
+            val y = match.groupValues[2].toInt()
+            Pair(x,y)
+        }.toList()
+
+private fun calculateResult(multiplications: List<Pair<Int, Int>>): Int =
+     multiplications
         .sumOf { (x, y) -> x * y }
-}
+
+private fun removeDonts(input: String): String =
+    input
+        .replace("\n", "|") // replace newlines with a separator
+        .replace(DELETE_DONT_REGEX, "") // remove everything after 'don't' until to the next 'do'
 
