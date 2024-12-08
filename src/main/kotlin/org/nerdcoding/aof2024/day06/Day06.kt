4e7dbbf1b65905f5af2a9c36c840b85233cdc1ea
Tobias Koltsch <tobias@koltsch.de>
2024-12-08 18:26:47 +0100

    Add day06 - part2
    Very bad solution, but at least is working

--- src/main/kotlin/org/nerdcoding/aof2024/day06/Day06.kt
+++ src/main/kotlin/org/nerdcoding/aof2024/day06/Day06.kt
@@ -1,20 +1,14 @@
 package org.nerdcoding.aof2024.day06
 
+import org.nerdcoding.aof2024.day06.Map.MoveResult
 import java.io.File
 
 private const val INPUT_FILE_LOCATION = "./src/main/resources/day06/input.txt"
 
-private val map = Map(readInputFile())
 
 fun main() {
-    val startingGuardPosition = map.findStartingGuardPosition()
-
-    var currentGuardPosition: Map.GuardPosition? = startingGuardPosition
-    while (currentGuardPosition != null) {
-        currentGuardPosition = map.moveGuard(currentGuardPosition)
-
-    }
-    println("Visited positions part1: ${map.countVisitedPositions()}")
+    solvePart1()
+    solvePart2()
 }
 
 private fun readInputFile(): Array<CharArray> =
@@ -25,3 +19,43 @@ private fun readInputFile(): Array<CharArray> =
             }.toList().toTypedArray()
     }
 
+private fun solvePart1() {
+    val map = Map(readInputFile())
+    val startingGuardPosition = map.findStartingGuardPosition()
+
+    var moveResult = MoveResult(startingGuardPosition, Map.GuardState.NEW_POSITION)
+    while (moveResult.guardState == Map.GuardState.NEW_POSITION) {
+        moveResult = map.moveGuard(moveResult.guardPosition)
+    }
+    println("Visited positions part1: ${map.countVisitedPositions()}")
+}
+
+private fun solvePart2() {
+
+    val map = Map(readInputFile())
+
+    var loopCounter = 0
+    for (y in 0 until map.getMapSize()) {
+        for (x in 0 until map.getMapSize()) {
+            val newMap = Map(readInputFile())
+            if (newMap.findValueAtPosition(x, y) == '.') {
+                newMap.changeValueAtPosition(x, y)
+                if (moveGuardInMap(newMap) == Map.GuardState.IN_LOOP) {
+                    loopCounter += 1
+                }
+            }
+        }
+    }
+
+    println("Loop counter part2: $loopCounter")
+}
+
+private fun moveGuardInMap(map: Map): Map.GuardState {
+    val startingGuardPosition = map.findStartingGuardPosition()
+
+    var moveResult = MoveResult(startingGuardPosition, Map.GuardState.NEW_POSITION)
+    while (moveResult.guardState == Map.GuardState.NEW_POSITION) {
+        moveResult = map.moveGuard(moveResult.guardPosition)
+    }
+    return moveResult.guardState
+}
--- src/main/kotlin/org/nerdcoding/aof2024/day06/Map.kt
+++ src/main/kotlin/org/nerdcoding/aof2024/day06/Map.kt
@@ -5,9 +5,11 @@ import org.nerdcoding.aof2024.day06.Map.MoveDirection.LEFT
 import org.nerdcoding.aof2024.day06.Map.MoveDirection.RIGHT
 import org.nerdcoding.aof2024.day06.Map.MoveDirection.UP
 
+
 class Map(private val map: Array<CharArray>)  {
 
     private val guardDirections = setOf('^', '>', 'v', '<')
+    private val visited = mutableMapOf<Pair<Int, Int>, MutableList<MoveDirection>>()
 
     enum class MoveDirection {
         UP,
@@ -24,6 +26,12 @@ class Map(private val map: Array<CharArray>)  {
             }
     }
 
+    enum class GuardState {
+        NEW_POSITION,
+        OUT_OF_MAP,
+        IN_LOOP
+    }
+
     fun printMap() =
         map.forEach { row -> println(row.joinToString(" ")) }
 
@@ -31,6 +39,16 @@ class Map(private val map: Array<CharArray>)  {
         map.sumOf {
             row -> row.count { it == 'X' }
         }
+
+    fun getMapSize() =
+        map.size
+
+    fun findValueAtPosition(x: Int, y: Int) =
+        map[y][x]
+
+    fun changeValueAtPosition(x: Int, y: Int) {
+        map[y][x] = '#'
+    }
 
     fun findStartingGuardPosition(): GuardPosition {
         for (y in map.indices) {
@@ -44,22 +62,37 @@ class Map(private val map: Array<CharArray>)  {
         throw IllegalArgumentException("map without starting point of guard")
     }
 
-    fun moveGuard(currentGuardPosition: GuardPosition): GuardPosition? {
+    fun moveGuard(currentGuardPosition: GuardPosition): MoveResult {
         val possibleNextGuardPosition = determineNextGuardPosition(currentGuardPosition)
         return if (isOutsideOfMap(possibleNextGuardPosition)) {
             // we left the map, mark current point in map as visited and we are finished
             markAsVisited(currentGuardPosition)
-            null
+            MoveResult(
+                possibleNextGuardPosition,
+                GuardState.OUT_OF_MAP
+            )
+        } else if (isInLoop(possibleNextGuardPosition)) {
+            // we visited a position for the second time
+            MoveResult(
+                possibleNextGuardPosition,
+                GuardState.IN_LOOP
+            )
         } else if (isNotBlocked(possibleNextGuardPosition)) {
             // mark current point in map as visited and move to next point
             markAsVisited(currentGuardPosition)
-            possibleNextGuardPosition
+            MoveResult(
+                possibleNextGuardPosition,
+                GuardState.NEW_POSITION
+            )
         } else {
             // next point is blocked, stay on current point and change direction
-            GuardPosition(
-                currentGuardPosition.x,
-                currentGuardPosition.y,
-                currentGuardPosition.direction.changeDirection()
+            MoveResult(
+                GuardPosition(
+                    currentGuardPosition.x,
+                    currentGuardPosition.y,
+                    currentGuardPosition.direction.changeDirection()
+                ),
+                GuardState.NEW_POSITION
             )
         }
     }
@@ -100,16 +133,29 @@ class Map(private val map: Array<CharArray>)  {
     private fun isOutsideOfMap(guardPosition: GuardPosition): Boolean =
         guardPosition.y !in map.indices || guardPosition.x !in map[guardPosition.y].indices
 
+    private fun isInLoop(guardPosition: GuardPosition): Boolean =
+        visited.values.any { list ->
+            list.size != list.toSet().size
+        }
+
     private fun isNotBlocked(guardPosition: GuardPosition): Boolean =
         map[guardPosition.y][guardPosition.x] != '#'
 
-    private fun markAsVisited(guardPosition: GuardPosition): Unit {
+    private fun markAsVisited(guardPosition: GuardPosition) {
         map[guardPosition.y][guardPosition.x] = 'X'
+
+        if (!visited.containsKey(Pair(guardPosition.x, guardPosition.y))) {
+            visited[Pair(guardPosition.x, guardPosition.y)] = mutableListOf()
+        }
+        visited[Pair(guardPosition.x, guardPosition.y)]?.add(guardPosition.direction)
+
     }
 
 
 
    class GuardPosition(val x: Int, val y: Int, val direction: MoveDirection)
+
+   class MoveResult(val guardPosition: GuardPosition, val guardState: GuardState)
 
 }
 
