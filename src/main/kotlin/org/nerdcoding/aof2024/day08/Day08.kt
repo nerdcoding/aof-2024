726835fb477513e6ba6e987bb31d02b272762200
Tobias Koltsch <tobias@koltsch.de>
2024-12-09 07:12:26 +0100

    Add day08 - part1

---
+++ src/main/kotlin/org/nerdcoding/aof2024/day08/Day08.kt
@@ -0,0 +1,57 @@
+package org.nerdcoding.aof2024.day08
+
+import java.io.File
+
+private const val INPUT_FILE_LOCATION = "./src/main/resources/day08/input.txt"
+
+val inputLines = File(INPUT_FILE_LOCATION).readLines()
+val frequencyPoints = readInputFile()
+
+fun main() {
+    val resultPart1 = countAntiNodes(::generateAntiNodes)
+
+    println("Result part1 $resultPart1")
+
+}
+
+private fun readInputFile(): Map<Char, List<Pair<Int, Int>>> {
+    // Maps all frequencies (Char) to points on the input puzzle (Pair<Int, Int>)
+    return inputLines.flatMapIndexed { y, row ->
+        row.mapIndexed { x, frequency ->
+            if (frequency != '.') {
+                frequency to Pair(x, y)
+            } else {
+                null
+            }
+        }
+    }.filterNotNull()
+        .groupBy({ it.first }, { it.second })
+}
+
+private fun countAntiNodes(generator: (Pair<Int, Int>, Pair<Int, Int>, Pair<Int, Int>) -> Set<Pair<Int, Int>>): Int =
+    frequencyPoints.values.flatMap {
+        pointList -> pointList.flatMapIndexed {
+            index, point1 -> pointList.drop(index + 1)
+                .flatMap {
+                    point2 -> generator.invoke(point1, point2, point1 - point2)
+                }
+        }.filter { it.isOnOriginalGrid() }
+    }.toSet().size
+
+private fun generateAntiNodes(
+        point1: Pair<Int, Int>,
+        point2: Pair<Int, Int>,
+        diff: Pair<Int, Int>): Set<Pair<Int, Int>> =
+    if (point1.second > point2.second) {
+        setOf(point1 - diff, point2 + diff)
+    } else {
+        setOf(point1 + diff, point2 - diff)
+    }
+
+
+fun Pair<Int, Int>.isOnOriginalGrid(): Boolean =
+    second in inputLines.indices && first in inputLines[second].indices
+operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
+    Pair(first + other.first, second + other.second)
+operator fun Pair<Int, Int>.minus(other: Pair<Int, Int>): Pair<Int, Int> =
+    Pair(first - other.first, second - other.second)
